b7357d7704180d66fa2e252f155545972681ed3e
newasad <asadhmeed2@gmail.com>
2022-06-09 20:42:10 +0300

    getGitHubUser

--- api/controller/user.controller.ts
+++ api/controller/user.controller.ts
@@ -1,7 +1,21 @@
+import { getGitHubUser } from "api/services/github.service"
 import { Response,Request } from "express"
-const githubFunc = (req : Request, res : Response) => {
+const githubFunc = async (req : Request, res : Response) => {
+    const {code}=req.query
+    const gitHubUser = await getGitHubUser(code as string)
+}
+
+const refreshHandler = async (req : Request, res : Response) => {
+
+}
+
+const logoutHandler = (req : Request, res : Response) => {
 
 }
 
 
-export {githubFunc}
+export {
+    githubFunc,
+    refreshHandler,
+    logoutHandler
+}
--- api/routes/user.route.ts
+++ api/routes/user.route.ts
@@ -1,4 +1,4 @@
-import { githubFunc } from "api/controller/user.controller";
+import { githubFunc, logoutHandler, refreshHandler } from "api/controller/user.controller";
 import  express  from "express";
 
 
@@ -6,6 +6,10 @@ const router = express.Router();
 
 
 router.get('/github', githubFunc)
+
+router.post('/refresh', refreshHandler)
+
+router.post("/logout",logoutHandler)
 
 
 export default router;
